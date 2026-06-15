c86b77ae3f910fb9d70443bd7cd3d06706d3cef6
Henrique Eduardo <henrique.eduardo.abrantes.moura@gmail.com>
2026-06-15 08:40:53 -0300

    fix: corrige overlap no typescript em PublicLayout

--- src/pages/client/ProfilePage.tsx
+++ src/pages/client/ProfilePage.tsx
@@ -211,6 +211,7 @@ const ProfilePage = () => {
         </div>
       </div>
 
+      <div className="px-6 md:px-12 mt-8 relative z-10">
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
           {/* Form Column */}
           <div className="lg:col-span-2 space-y-6">
